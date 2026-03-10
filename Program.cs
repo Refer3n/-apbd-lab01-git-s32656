0fbec74b92476f6a4bcc88c4b35ed5976bf7011f
Oleksandr Lavrynenko <s32656@pjwstk.edu.pl>
2026-03-10 13:35:35 +0100

    Change variable name to nums

--- Program.cs
+++ Program.cs
@@ -2,15 +2,15 @@
 
 using Tutorial2;
 
-var numbers = new double[10];
+var nums = new double[10];
     
 Console.WriteLine("=== Number Input ===");
 
 for (var i = 0; i < 10; i++)
 {
     Console.Write($"[{i + 1}/10] Enter a number: ");
-    Double.TryParse(Console.ReadLine(), out numbers[i]);
+    Double.TryParse(Console.ReadLine(), out nums[i]);
 }
 
-Console.WriteLine("Average: " + StatisticHelper.CalculateAverage(numbers));
-Console.WriteLine("Max: " + StatisticHelper.CalculateMax(numbers));
+Console.WriteLine("Average: " + StatisticHelper.CalculateAverage(nums));
+Console.WriteLine("Max: " + StatisticHelper.CalculateMax(nums));
