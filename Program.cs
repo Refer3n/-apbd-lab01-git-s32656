7480e011ab2c66630dd5f450ae82e625f5a34963
Oleksandr Lavrynenko <s32656@pjwstk.edu.pl>
2026-03-10 13:18:17 +0100

    Add average output

--- Program.cs
+++ Program.cs
@@ -1,5 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+using Tutorial2;
+
 var numbers = new double[10];
     
 Console.WriteLine("=== Number Input ===");
@@ -9,3 +11,5 @@ for (var i = 0; i < 10; i++)
     Console.Write($"[{i + 1}/10] Enter a number: ");
     Double.TryParse(Console.ReadLine(), out numbers[i]);
 }
+
+Console.WriteLine("Average: " + StatisticHelper.CalculateAverage(numbers));
