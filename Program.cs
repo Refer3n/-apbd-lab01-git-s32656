19164e1d4db25df6f814e148cf142415f4337815
Oleksandr Lavrynenko <s32656@pjwstk.edu.pl>
2026-03-10 12:59:02 +0100

    Add promt for user input

--- Program.cs
+++ Program.cs
@@ -1,3 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Hello, World!");
+var numbers = new double[10];
+
+for (var i = 0; i < 10; i++)
+{
+    Console.WriteLine("Enter some number: ");
+    Double.TryParse(Console.ReadLine(), out numbers[i]);
+}
