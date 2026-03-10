2dc351a132c82cef2ee9b8c6bf3c5894fe4084de
Oleksandr Lavrynenko <s32656@pjwstk.edu.pl>
2026-03-10 13:03:44 +0100

    Add styling for user input

--- Program.cs
+++ Program.cs
@@ -1,9 +1,11 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 var numbers = new double[10];
+    
+Console.WriteLine("=== Number Input ===");
 
 for (var i = 0; i < 10; i++)
 {
-    Console.WriteLine("Enter some number: ");
+    Console.Write($"[{i + 1}/10] Enter a number: ");
     Double.TryParse(Console.ReadLine(), out numbers[i]);
 }
